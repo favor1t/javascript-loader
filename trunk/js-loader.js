824bf6f30bd2686f85931bcd3411589da48cbc11
george.mandis@gmail.com <george.mandis@gmail.com@17738718-3f89-11de-8624-41c2277f587d>
2009-05-13 07:09:12 +0000

    Added js-loader.js.  Whoop-dee-doo.

---
+++ trunk/js-loader.js
@@ -0,0 +1,13 @@
+var script = (function(){
+    var src=document.getElementsByTagName('script');
+    return src[src.length-1].getAttribute("src");
+	})();
+var q = script.indexOf('?');
+if (q != -1) {
+	path = script.substring(0, script.lastIndexOf("/")+1);
+	filename = script.substring(path.length,q);
+	files = script.substring(q+1, script.length).split("|");
+	}
+for(file in files) {
+    document.write("<script type='text/javascript'src='"+path+files[file]+".js'></script>");
+	}
