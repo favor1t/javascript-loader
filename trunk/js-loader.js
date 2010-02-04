fb473a65965a5546dd083e677ace06bec330c8d7
george.mandis@gmail.com <george.mandis@gmail.com@17738718-3f89-11de-8624-41c2277f587d>
2010-02-04 09:52:22 +0000

    Updated; can either list additional scripts when callign js-loader, or can use the JSLoader object to load them.  Whatever man.

--- trunk/js-loader.js
+++ trunk/js-loader.js
@@ -1,3 +1,6 @@
+/*
+	JavaScript Loader
+*/
 var script = (function(){
     var src=document.getElementsByTagName('script');
     return src[src.length-1].getAttribute("src");
@@ -7,7 +10,16 @@ if (q != -1) {
 	path = script.substring(0, script.lastIndexOf("/")+1);
 	filename = script.substring(path.length,q);
 	files = script.substring(q+1, script.length).split("|");
+	for(file in files) {
+	    document.write("<script type='text/javascript'src='"+path+files[file]+".js'></script>");
+		}
 	}
-for(file in files) {
-    document.write("<script type='text/javascript'src='"+path+files[file]+".js'></script>");
+var JSLoader = {
+	load: function(){
+		for (i in arguments) {
+		    document.write("<script type='text/javascript'src='"+arguments[i]+".js'></script>");			
+			}
+		}	
 	}
+
+
